fe0132ae201d3b6ab4c6896af8b3b24f6cff91fa
Ryan Boris <19439753+secantsquared@users.noreply.github.com>
2020-04-26 18:43:40 -0400

    :tada: Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useEffect, useState, useRef } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const DropDown = props => {
+  return (
+    <>
+      <select onChange={e => props.getId(e)} name="wordcloud" id="wordcloud">
+        {props.options.map((o, i) => (
+          <option id={props.id} key={i}>
+            {o}
+          </option>
+        ))}
+      </select>
+    </>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const WordCloud = () => {
+  const [data, setData] = useState([])
+  const [dataRaw, setDataRaw] = useState()
+  const [date, setDate] = useState(null)
+  const [newspaper, setNewsPaper] = useState(null)
+
+  const getId = e => {
+    if (e.target.selectedOptions[0].id === 'date') {
+      setDate(e.target.selectedOptions[0].textContent)
+    } else {
+      setNewsPaper(e.target.selectedOptions[0].textContent)
+    }
+  }
+  useEffect(() => {
+    fetch('http://localhost:3000/data.json')
+      .then(res => res.json())
+      .then(data => {
+        setDataRaw(data)
+        const values = Object.values(data)
+        setData(values.map((val, i) => ({ ...val, id: i })))
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  const newspapers1 = data.map(d => d.newspaper)
+  const newspapersSet = new Set(newspapers1)
+  const newspapers = Array.from(newspapersSet)
+  const datesDirty = data.map(d => d.date)
+  const datesSet = new Set(datesDirty)
+  const dates = Array.from(datesSet)
+  return (
+    <div classname="App">
+      <DropDown options={newspapers} getId={getId} id="newspaper" />
+      <DropDown options={dates} getId={getId} id="date" />
+      {data
+        .filter(d => d.date === date && d.newspaper === newspaper)
+        .map((d, i) => {
+          return <img key={i} src={d['IMG URL'] || null} alt="wordcloud" />
+        })}
+    </div>
+  )
+}
+
+ReactDOM.render(<WordCloud />, document.getElementById('root'))
